a302b84e445146a05df082caa8b06e85abe5b783
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2026-08-22 04:30:43 +0300

    feat: integrate MongoDB with Mongoose and update course controller methods

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,53 +1,56 @@
 import { validationResult } from "express-validator";
-import { courses } from "../data/courses.js";
+import { courseModel as Course } from "../models/course.model.js";
 
-const getCourses = (req, res) => {
+const getCourses = async (req, res) => {
+  const courses = await Course.find();
   res.json(courses);
 };
 
-const getCourseById = (req, res) => {
-  const course = courses.find((course) => course.id === +req.params.courseId);
-
-  if (!course) return res.status(404).send({ message: "Course not found" });
-  res.json(course);
+const getCourseById = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.courseId);
+    if (!course) return res.status(404).send({ message: "Course not found" });
+    return res.json(course);
+  } catch (error) {
+    return res.status(400).send({ message: "invalid Object Id" });
+  }
 };
 
-const addCourse = (req, res) => {
+const addCourse = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json(errors.array());
 
-  const course = {
-    id: courses.length + 1,
-    ...req.body,
-  };
+  const newCourse = new Course(req.body);
+  await newCourse.save();
 
-  courses.push(course);
-
-  res.status(201).json(course);
+  res.status(201).json(newCourse);
 };
 
-const updateCourse = (req, res) => {
-  const id = +req.params.courseId;
-
-  let course = courses.find((course) => course.id === id);
-  if (!course) return res.status(404).send({ message: "Course not found" });
-
-  course = { ...course, ...req.body };
-  res.status(200).json(course);
-};
-
-const deleteCourse = (req, res) => {
-  const id = +req.params.courseId;
-
-  const index = courses.findIndex((course) => course.id === id);
-
-  if (index === -1) {
-    return res.status(404).json({ message: "Course not found" });
+const updateCourse = async (req, res) => {
+  try {
+    const updatedCourse = await Course.updateOne(
+      { _id: req.params.courseId },
+      {
+        $set: { ...req.body },
+      },
+    );
+    if (!updatedCourse)
+      return res.status(404).send({ message: "Course not found" });
+    return res.status(200).json({ message: "Course updated successfully" });
+  } catch (e) {
+    return res.status(400).json({ error: e });
   }
+};
 
-  courses.splice(index, 1);
-
-  res.status(200).json(courses);
+const deleteCourse = async (req, res) => {
+  try {
+    const deletedCourse = await Course.findByIdAndDelete({ _id: req.params.courseId });
+    if (!deletedCourse)
+      return res.status(404).send({ message: "Course not found" });
+    return res.status(200).json({ message: "Course deleted successfully" });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
 };
 
 export default {
--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 import express from "express";
 import { router as coursesRouter } from "./routes/courses.route.js";
+import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const url = process.env.DB_URL;
+
+// MongoDB
+// const client = new MongoClient(url);
+
+// const main = async () => {
+//   await client.connect();
+//   const db = client.db("codezone");
+//   const collection = db.collection("courses");
+// //   await collection.insertOne({
+// //     title: "CSS course",
+// //     price: 50,
+// //   });
+//   const data = await collection.find().toArray();
+// };
+// main();
+
+// Mongoose
+
+mongoose.connect(url).then(() => {
+  console.log("mongoDB connected successfully");
+});
 
 const app = express();
 app.use(express.json());
